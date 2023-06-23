5c62271b319feed55d767a5555f01a1315f4416c
Lazzzer <dev@lazarpavicevic.ch>
2023-06-23 19:36:53 +0200

    feat: styling of for the page

--- components/text-recognition-pipeline.tsx
+++ components/text-recognition-pipeline.tsx
@@ -1,51 +1,25 @@
+"use client";
+
 import { buttonVariants } from "./ui/button";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { headers } from "next/headers";
 import { Textarea } from "./ui/textarea";
+import { useState } from "react";
+import { Icons } from "./icons";
+import { Balancer } from "react-wrap-balancer";
 
-async function getS3ObjectUrl(uuid: string) {
-  const res = await fetch(
-    `${process.env.APP_URL}/api/signed-url?uuid=${uuid}`,
-    {
-      method: "GET",
-      headers: {
-        Cookie: headers().get("cookie") ?? "",
-      },
-    }
-  );
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch url");
-  }
-
-  return res.json();
-}
-
-async function getText(url: string) {
-  const res = await fetch(`${process.env.APP_URL}/api/text-recognition`, {
-    method: "POST",
-    headers: {
-      Cookie: headers().get("cookie") ?? "",
-    },
-    body: JSON.stringify({ url }),
-  });
-
-  if (!res.ok) {
-    throw new Error("Failed to fetch text");
-  }
-
-  return res.json();
-}
-
-export default async function TextRecognitionPipeline({
+export default function TextRecognitionPipeline({
   uuid,
+  url,
+  text,
+  filename,
 }: {
   uuid: string;
+  url: string;
+  text: string;
+  filename: string;
 }) {
-  const { url, filename } = await getS3ObjectUrl(uuid);
-  const { text } = await getText(url);
-
+  const [verifiedText, setVerifiedText] = useState(text);
   return (
     <div className="mx-4 mb-4 flex flex-col flex-grow">
       <div className="flex flex-1 items-center justify-center gap-x-10">
@@ -54,22 +28,52 @@ export default async function TextRecognitionPipeline({
           type="application/pdf"
           className="bg-slate-900 rounded-lg p-2 w-3/8 h-6/7 2xl:w-3/10 2xl:h-3/4"
         />
-        <div>
-          <Textarea
-            value={text}
-            // onChange={() => {
-            //   // TODO: Put in its own client component
-            // }}
-            className="w-96 h-96"
-            placeholder="Type your message here."
-          />
+        <div className="w-3/8 h-6/7 2xl:w-3/10 2xl:h-3/4 flex flex-col justify-between">
+          <div className="w-full h-3/4">
+            <h1 className="mb-1 text-sm text-slate-700 overflow-hidden">
+              Text from{" "}
+              <span className="text-ellipsis font-semibold">{filename}</span>
+            </h1>
+            <Textarea
+              value={text}
+              onChange={(e) => setVerifiedText(e.target.value)}
+              className="w-full h-full rounded-lg"
+              placeholder="Type your message here."
+            />
 
-          <Link
-            className={cn(buttonVariants(), "w-full")}
-            href={`/data-extraction/${uuid}`}
-          >
-            Continue
-          </Link>
+            <div className="flex gap-2 items-center justify-center w-full mt-4">
+              <Icons.help
+                width={20}
+                height={20}
+                className="inline-block text-slate-500 flex-none"
+              />
+              <Balancer>
+                <p className="text-sm text-slate-500">
+                  Please make sure that the text above matches the text in the
+                  PDF. Feel free to remove or edit any part of the text.
+                </p>
+              </Balancer>
+            </div>
+          </div>
+
+          <div className="flex gap-2 justify-end">
+            <Link
+              className={cn(
+                buttonVariants({
+                  variant: "secondary",
+                })
+              )}
+              href={`/data-extraction/${uuid}`}
+            >
+              Cancel
+            </Link>
+            <Link
+              className={cn(buttonVariants(), "w-48")}
+              href={`/data-extraction/${uuid}`}
+            >
+              Confirm & Continue
+            </Link>
+          </div>
         </div>
       </div>
     </div>
