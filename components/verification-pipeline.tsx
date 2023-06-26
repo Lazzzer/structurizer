fcc6b6655e6720d2fee9c5f02089ef26617a2353
Lazzzer <dev@lazarpavicevic.ch>
2023-06-26 23:23:17 +0200

    feat: add layout

--- components/verification-pipeline.tsx
+++ components/verification-pipeline.tsx
@@ -1,8 +1,9 @@
 "use client";
-import { Button, buttonVariants } from "./ui/button";
-import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { useStepStore } from "@/lib/store";
+import { Icons } from "./icons";
+import { Button, buttonVariants } from "./ui/button";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "./ui/tabs";
+import { cn } from "@/lib/utils";
 
 export default function VerificationPipeline({
   uuid,
@@ -21,25 +22,85 @@ export default function VerificationPipeline({
 }) {
   return (
     <div className="mx-4 mb-4 flex flex-col flex-grow">
-      <div className="flex flex-1 items-center justify-center gap-x-10">
-        <object
-          data={`${url}#toolbar=1&navpanes=0&statusbar=0&scrollbar=1&view=fit`}
-          type="application/pdf"
-          className="bg-slate-900 rounded-lg p-2 w-3/8 h-6/7 2xl:w-3/10 2xl:h-3/4"
-        />
+      <div className="flex flex-1 flex-col justify-center">
+        <div
+          style={{
+            height: "80%",
+          }}
+          className="w-full flex justify-center items-start gap-x-10 -mt-12"
+        >
+          <Tabs
+            style={{
+              height: "calc(100% - 48px)",
+            }}
+            defaultValue="pdf"
+            className="w-3/8 2xl:w-3/10"
+          >
+            <TabsList className="grid w-full grid-cols-2">
+              <TabsTrigger value="pdf">PDF View</TabsTrigger>
+              <TabsTrigger value="text">Text View</TabsTrigger>
+            </TabsList>
+            <TabsContent
+              value="pdf"
+              className="p-2 w-full h-full bg-slate-900 rounded-lg"
+            >
+              <object
+                data={`${url}#toolbar=1&navpanes=0&statusbar=0&scrollbar=1&view=fitH`}
+                type="application/pdf"
+                className="w-full h-full rounded-md"
+              />
+            </TabsContent>
+            <TabsContent
+              value="text"
+              className="w-full h-full border p-4 border-slate-200 rounded-lg"
+            >
+              <div
+                style={{
+                  maxHeight: "57vh", // preformatted elements really don't like height: 100%, so here we are...
+                }}
+                className="w-full text-xs text-slate-700 whitespace-break-spaces overflow-auto"
+              >
+                {text}
+              </div>
+            </TabsContent>
+          </Tabs>
+          <div className="w-2/5 h-full flex flex-col justify-center relative">
+            <h1 className="text-2xl mb-1 font-bold text-slate-800">
+              Extracted Data
+            </h1>
+            <div className="bg-red-200 w-full h-full">json structure</div>
+            <div className="flex justify-end gap-2 items-center absolute -bottom-12 right-0">
+              <Link
+                className={cn(
+                  buttonVariants({
+                    variant: "secondary",
+                  })
+                )}
+                href="/dashboard"
+              >
+                Cancel
+              </Link>
+              <Button
+                variant={"secondary"}
+                className="relative inline-flex w-40 overflow-hidden bg-slate-100 p-[1.5px] group"
+              >
+                <span className="absolute inset-[-1000%] group-hover:animate-[spin_2s_linear_infinite] bg-slate-100 group-hover:bg-[conic-gradient(from_90deg_at_50%_50%,#FD95FF_0%,#FD95FF_50%,#00E1F0_100%)]" />
+                <span className="inline-flex h-full w-full items-center rounded justify-center bg-slate-100 px-3 py-1 backdrop-blur-3xl">
+                  <Icons.sparkles
+                    width={18}
+                    height={18}
+                    className="inline-block mr-2 group-hover:pulse"
+                  />
+                  Analyze
+                </span>
+              </Button>
+              <Button className="w-40" onClick={() => {}}>
+                Confirm
+              </Button>
+            </div>
+          </div>
+        </div>
       </div>
-      <Button
-        onClick={() =>
-          useStepStore.setState(() => ({
-            status: "complete",
-          }))
-        }
-      >
-        Process End
-      </Button>
-      <Link className={cn(buttonVariants(), "mb-4 mx-4")} href={"/dashboard"}>
-        Confirm
-      </Link>
     </div>
   );
 }
