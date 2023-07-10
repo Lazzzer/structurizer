5e8b5ad97a91479a35eade7391be1d2148530e85
Lazzzer <dev@lazarpavicevic.ch>
2023-07-10 23:48:21 +0200

    refactor: use generic sheet skeleton

--- components/sheet-invoice.tsx
+++ components/sheet-invoice.tsx
@@ -19,10 +19,10 @@ import {
 } from "./ui/alert-dialog";
 import { deleteExtraction, getObjectUrl } from "@/lib/client-requests";
 import { SheetInvoiceEditor } from "./sheet-invoice-editor";
-import { Skeleton } from "./ui/skeleton";
 import { SheetError } from "./sheet-error";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
 import { mapCurrency } from "@/lib/utils";
+import { SheetSkeleton } from "./sheet-skeleton";
 
 interface SheetInvoiceProps {
   id: string;
@@ -206,10 +206,10 @@ export function SheetInvoice({ id, children }: SheetInvoiceProps) {
                 <AlertDialog>
                   <AlertDialogContent>
                     <AlertDialogHeader>
-                      <AlertDialogTitle>Delete Receipt</AlertDialogTitle>
+                      <AlertDialogTitle>Delete Invoice</AlertDialogTitle>
                       <AlertDialogDescription>
                         Are you sure? This will permanently delete the current
-                        receipt and remove its associated file and extraction.
+                        invoice and remove its associated file and extraction.
                         This action cannot be undone.
                       </AlertDialogDescription>
                     </AlertDialogHeader>
@@ -249,67 +249,7 @@ export function SheetInvoice({ id, children }: SheetInvoiceProps) {
             )}
           </div>
         ) : (
-          <div className="h-full w-full my-4 relative">
-            <Skeleton className="h-8 w-[100px] rounded-md" />
-            <div className="w-full">
-              <div className="mt-10 flex justify-between items-center">
-                <Skeleton className="h-6 w-[100px] rounded-md" />
-                <Skeleton className="h-4 w-[100px] rounded" />
-              </div>
-              <div className="mt-6 flex justify-between">
-                <div>
-                  <Skeleton className="h-5 w-[65px] rounded mb-1" />
-                  <Skeleton className="h-4 w-[150px] rounded" />
-                </div>
-                <div className="flex flex-col items-end">
-                  <Skeleton className="h-5 w-[65px] rounded mb-1" />
-                  <Skeleton className="h-4 w-[50px] rounded" />
-                </div>
-              </div>
-              <div className="mt-3">
-                <div>
-                  <Skeleton className="h-5 w-[80px] rounded mb-1" />
-                  <Skeleton className="h-4 w-[150px] rounded" />
-                </div>
-              </div>
-              <div className="mt-6">
-                <div>
-                  <Skeleton className="h-6 w-[65px] rounded-md" />
-                  <div className="mt-2 w-full">
-                    <Skeleton className="h-5 w-full rounded-md" />
-                  </div>
-                  <div className="h-52 2xl:h-96 overflow-scroll mt-4">
-                    <Skeleton className="h-full w-full rounded-lg" />
-                  </div>
-                </div>
-              </div>
-              <div className="mt-6">
-                <div className="flex justify-between items-center">
-                  <Skeleton className="h-5 w-[65px] rounded" />
-                  <Skeleton className="h-4 w-[100px] rounded" />
-                </div>
-                <div className="flex justify-between items-center mt-3">
-                  <Skeleton className="h-5 w-[65px] rounded" />
-                  <Skeleton className="h-4 w-[100px] rounded" />
-                </div>
-                <div className="flex justify-between items-center mt-3">
-                  <Skeleton className="h-5 w-[65px] rounded" />
-                  <Skeleton className="h-4 w-[100px] rounded" />
-                </div>
-              </div>
-              <div className="mt-8 flex flex-col items-end">
-                <Skeleton className="h-8 w-[65px] rounded-md mb-2" />
-                <Skeleton className="h-5 w-[100px] rounded" />
-              </div>
-            </div>
-            <div className="w-full flex gap-2 items-center justify-between absolute bottom-2 right-0">
-              <Skeleton className="h-6 w-[125px] rounded-md" />
-              <div className="flex gap-2">
-                <Skeleton className="h-10 w-20 rounded-md" />
-                <Skeleton className="h-10 w-20 rounded-md" />
-              </div>
-            </div>
-          </div>
+          <SheetSkeleton />
         )}
       </SheetContent>
     </Sheet>
--- components/sheet-receipt.tsx
+++ components/sheet-receipt.tsx
@@ -19,9 +19,9 @@ import {
 } from "./ui/alert-dialog";
 import { deleteExtraction, getObjectUrl } from "@/lib/client-requests";
 import { SheetReceiptEditor } from "./sheet-receipt-editor";
-import { Skeleton } from "./ui/skeleton";
 import { SheetError } from "./sheet-error";
 import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import { SheetSkeleton } from "./sheet-skeleton";
 
 interface SheetReceiptProps {
   id: string;
@@ -256,67 +256,7 @@ export function SheetReceipt({ id, children }: SheetReceiptProps) {
             )}
           </div>
         ) : (
-          <div className="h-full w-full my-4 relative">
-            <Skeleton className="h-8 w-[100px] rounded-md" />
-            <div className="w-full">
-              <div className="mt-10 flex justify-between items-center">
-                <Skeleton className="h-6 w-[100px] rounded-md" />
-                <Skeleton className="h-4 w-[100px] rounded" />
-              </div>
-              <div className="mt-6 flex justify-between">
-                <div>
-                  <Skeleton className="h-5 w-[65px] rounded mb-1" />
-                  <Skeleton className="h-4 w-[150px] rounded" />
-                </div>
-                <div className="flex flex-col items-end">
-                  <Skeleton className="h-5 w-[65px] rounded mb-1" />
-                  <Skeleton className="h-4 w-[50px] rounded" />
-                </div>
-              </div>
-              <div className="mt-3">
-                <div>
-                  <Skeleton className="h-5 w-[80px] rounded mb-1" />
-                  <Skeleton className="h-4 w-[150px] rounded" />
-                </div>
-              </div>
-              <div className="mt-6">
-                <div>
-                  <Skeleton className="h-6 w-[65px] rounded-md" />
-                  <div className="mt-2 w-full">
-                    <Skeleton className="h-5 w-full rounded-md" />
-                  </div>
-                  <div className="h-52 2xl:h-96 overflow-scroll mt-4">
-                    <Skeleton className="h-full w-full rounded-lg" />
-                  </div>
-                </div>
-              </div>
-              <div className="mt-6">
-                <div className="flex justify-between items-center">
-                  <Skeleton className="h-5 w-[65px] rounded" />
-                  <Skeleton className="h-4 w-[100px] rounded" />
-                </div>
-                <div className="flex justify-between items-center mt-3">
-                  <Skeleton className="h-5 w-[65px] rounded" />
-                  <Skeleton className="h-4 w-[100px] rounded" />
-                </div>
-                <div className="flex justify-between items-center mt-3">
-                  <Skeleton className="h-5 w-[65px] rounded" />
-                  <Skeleton className="h-4 w-[100px] rounded" />
-                </div>
-              </div>
-              <div className="mt-8 flex flex-col items-end">
-                <Skeleton className="h-8 w-[65px] rounded-md mb-2" />
-                <Skeleton className="h-5 w-[100px] rounded" />
-              </div>
-            </div>
-            <div className="w-full flex gap-2 items-center justify-between absolute bottom-2 right-0">
-              <Skeleton className="h-6 w-[125px] rounded-md" />
-              <div className="flex gap-2">
-                <Skeleton className="h-10 w-20 rounded-md" />
-                <Skeleton className="h-10 w-20 rounded-md" />
-              </div>
-            </div>
-          </div>
+          <SheetSkeleton />
         )}
       </SheetContent>
     </Sheet>
