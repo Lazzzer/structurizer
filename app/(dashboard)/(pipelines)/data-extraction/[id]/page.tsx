bb8e32e53cf7174e5cd0fe9ae719d86e1773dfad
Lazzzer <dev@lazarpavicevic.ch>
2023-07-16 21:54:53 +0200

    feat: add tips for data-extraction and verification

--- app/(dashboard)/(pipelines)/data-extraction/[id]/page.tsx
+++ app/(dashboard)/(pipelines)/data-extraction/[id]/page.tsx
@@ -1,3 +1,4 @@
+import { HelpPopover } from "@/components/ui/help-popover";
 import DataExtractionPipeline from "../components/data-extraction-pipeline";
 import { TopMainContent } from "@/components/top-main-content";
 import { getExtraction } from "@/lib/server-requests";
@@ -21,7 +22,28 @@ export default async function DataExtractionPipelinePage({
 
   return (
     <div className="flex flex-col h-full">
-      <TopMainContent title="Data Extraction" step={3} />
+      <TopMainContent title="Data Extraction" step={3}>
+        <HelpPopover
+          align="start"
+          iconClassName="w-6 h-6 text-slate-900 stroke-[2.5] -mt-1 ml-1.5 inline-block"
+          contentClassName="max-h-[500px]"
+        >
+          <article className="prose prose-sm leading-normal">
+            <h3 className="text-lg font-semibold">Data Extraction Pipeline</h3>
+            <p>The LLM conducts the data extraction process in two stages.</p>
+            <p>
+              Initially, it determines the category of the document. Once
+              categorized, the LLM uses a specific structure, such as a JSON
+              schema, that corresponds to the identified category to extract the
+              data.
+            </p>
+            <p>
+              You are given the option to select the category manually if the
+              classification fails or is incorrect.
+            </p>
+          </article>
+        </HelpPopover>
+      </TopMainContent>
       <DataExtractionPipeline id={params.id} text={data.text as string} />
     </div>
   );
--- app/(dashboard)/(pipelines)/verification/[id]/page.tsx
+++ app/(dashboard)/(pipelines)/verification/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getExtraction, getObjectUrl } from "@/lib/server-requests";
 import { Status } from "@prisma/client";
 import VerificationPipeline from "../components/verification-pipeline";
 import { Metadata } from "next";
+import { HelpPopover } from "@/components/ui/help-popover";
 
 export const metadata: Metadata = {
   title: "Verification",
@@ -23,7 +24,33 @@ export default async function VerificationPipelinePage({
   const { url } = await getObjectUrl(params.id);
   return (
     <div className="flex flex-col h-full">
-      <TopMainContent title="Verification" step={4} />
+      <TopMainContent title="Verification" step={4}>
+        <HelpPopover
+          align="start"
+          iconClassName="w-6 h-6 text-slate-900 stroke-[2.5] -mt-1 ml-1.5 inline-block"
+          contentClassName="max-h-[500px]"
+        >
+          <article className="prose prose-sm leading-normal">
+            <h3 className="text-lg font-semibold">Verification Pipeline</h3>
+            <p>This pipeline is the final step of the structuring process.</p>
+            <p>
+              LLMs may have generated inaccuracies or hallucinations during the
+              previous stages. This pipeline gives you the opportunity to have
+              the final say on the extracted data.
+            </p>
+            <p>
+              An additional feature you can use is automatic verification
+              provided by the LLM.
+            </p>
+            <p>
+              However, please be aware that it may{" "}
+              <u>generate false positives or overlook certain errors</u>. This
+              feature solely relies on the text for its analysis. So, subtle
+              errors within the text may likely go undetected.
+            </p>
+          </article>
+        </HelpPopover>
+      </TopMainContent>
       <VerificationPipeline
         id={params.id}
         url={url}
